a50e2708b6d821ef8840415d1dc7066c5aa885a0
janguenot <devnull@localhost>
2006-04-07 13:07:01 +0000

    Analyzer helper

---
+++ src/nxlucene/analyzer.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2006, Nuxeo SAS <http://www.nuxeo.com>
+# Author: Julien Anguenot <ja@nuxeo.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-13
+"""Analyzer helper
+
+$Id: core.py 31300 2006-03-15 03:10:04Z janguenot $
+"""
+
+import PyLucene
+
+analyzers_map = {
+    # XXX not complete
+    'standard' : PyLucene.StandardAnalyzer(),
+    'french'   : PyLucene.FrenchAnalyzer(),    
+    }
+
+def getAnalyzerById(analyzer_id):
+    if not analyzer_id in analyzers_map.keys():
+        analyzer_id = 'standard'
+    return analyzers_map.get(analyzer_id)
